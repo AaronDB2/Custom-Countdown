ee2db2a2328eedf5dfb294ef728dcde66748ab13
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-06 20:51:37 +0100

    Added the form functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+
+let countdownTitle = '';
+let countdownDate = '';
+
+// Set Date Input Min with Today's Date
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+
+// Take Values from Form Input
+function updateCountdown (e) {
+	e.preventDefault();
+	countdownTitle = e.srcElement[0].value;
+	countdownDate = e.srcElement[1].value;
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
