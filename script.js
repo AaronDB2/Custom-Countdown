f64435d702103331035e31cca08f17838a321efd
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-06 21:11:28 +0100

    Made the countdown UI use the values from the form

--- script.js
+++ script.js
@@ -2,19 +2,55 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('coountdown-button');
+const timeElements = document.querySelectorAll('span');
 
 let countdownTitle = '';
 let countdownDate = '';
+let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Populate Countdown / Complete UI
+function updateDOM() {
+	const now = new Date().getTime();
+	const distance = countdownValue - now;
+
+	const days = Math.floor(distance / day);
+	const hours = Math.floor((distance % day) / hour);
+	const minutes = Math.floor((distance % hour) / minute);
+	const seconds = Math.floor((distance % minute) / second);
+
+	// Populate Countdown
+	countdownTitle.textContent = `${countdownTitle}`;
+	timeElements[0].textContent = `${days}`;
+	timeElements[1].textContent = `${hours}`;
+	timeElements[2].textContent = `${minutes}`;
+	timeElements[3].textContent = `${seconds}`;
+
+	// Hide Input
+	inputContainer.hidden = true;
+	// Show Countdown
+	countdownEl.hidden = false;
+}
 
 // Take Values from Form Input
 function updateCountdown (e) {
 	e.preventDefault();
 	countdownTitle = e.srcElement[0].value;
 	countdownDate = e.srcElement[1].value;
+	// Get number version of current Date, updateDOM
+	countdownValue = new Date(countdownDate).getTime();
+	updateDOM();
 }
 
 // Event Listeners
